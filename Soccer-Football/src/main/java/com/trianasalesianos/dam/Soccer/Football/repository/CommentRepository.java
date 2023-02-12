36bdc0b84dec816dc38214840be49c7f7a9e509f
Alejandro <fernandez.goale21@triana.salesianos.edu>
2023-02-12 23:22:07 +0100

    forgot branches

--- Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trianasalesianos.dam.Soccer.Football.repository;
-
-import com.trianasalesianos.dam.Soccer.Football.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trianasalesianos.dam.Soccer.Football.repository;
-
-import com.trianasalesianos.dam.Soccer.Football.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
