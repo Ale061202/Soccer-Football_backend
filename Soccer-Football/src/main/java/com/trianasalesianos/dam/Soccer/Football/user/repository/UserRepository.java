aa4bd60a9ddb64030100b6052400a553018e6ce1
Alejandro <fernandez.goale21@triana.salesianos.edu>
2023-02-14 09:32:59 +0100

    problems fixed on branch post

---
+++ Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.trianasalesianos.dam.Soccer.Football.user.repository;
+
+import com.trianasalesianos.dam.Soccer.Football.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
