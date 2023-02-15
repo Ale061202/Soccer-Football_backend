753df2818769881a112211a8e5fe1f35352d6202
Alejandro <fernandez.goale21@triana.salesianos.edu>
2023-02-15 20:17:55 +0100

    SecurityConfig error

--- Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/security/PasswordEncoderConfig.java
+++ Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/security/PasswordEncoderConfig.java
@@ -1,4 +1,14 @@
 package com.trianasalesianos.dam.Soccer.Football.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+@Configuration
 public class PasswordEncoderConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
