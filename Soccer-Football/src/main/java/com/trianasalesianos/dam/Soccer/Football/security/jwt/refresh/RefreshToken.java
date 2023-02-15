21356961fcf22cfe84a71655cc112d3c7bc808a9
Alejandro <fernandez.goale21@triana.salesianos.edu>
2023-02-15 19:37:56 +0100

    security solved

--- Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/security/jwt/refresh/RefreshToken.java
+++ Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/security/jwt/refresh/RefreshToken.java
@@ -1,4 +1,37 @@
 package com.trianasalesianos.dam.Soccer.Football.security.jwt.refresh;
 
+import com.trianasalesianos.dam.Soccer.Football.user.model.User;
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class RefreshToken {
+
+    @Id
+    private UUID id;
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "user_id", columnDefinition = "uuid")
+    private User user;
+
+    @NaturalId
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    @CreatedDate
+    private Instant createdAt;
 }
--- Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/security/jwt/refresh/RefreshTokenRequest.java
+++ Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/security/jwt/refresh/RefreshTokenRequest.java
@@ -1,4 +1,14 @@
 package com.trianasalesianos.dam.Soccer.Football.security.jwt.refresh;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
 public class RefreshTokenRequest {
+
+    private String refreshToken;
 }
