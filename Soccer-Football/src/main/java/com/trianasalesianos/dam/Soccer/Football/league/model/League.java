f75461ecac3090ee9c2853aeed31766fd8ff75fb
Alejandro <fernandez.goale21@triana.salesianos.edu>
2023-02-21 20:34:31 +0100

    finding solution to league controller

--- Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/league/model/League.java
+++ Soccer-Football/src/main/java/com/trianasalesianos/dam/Soccer/Football/league/model/League.java
@@ -6,6 +6,7 @@ import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +22,8 @@ public class League {
     private Long id;
 
     private String league_name;
+
+    private LocalDateTime uploadDate = LocalDateTime.now();
 
     @OneToMany(fetch = FetchType.LAZY)
     private List<Team> teams = new ArrayList<>();
